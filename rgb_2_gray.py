068995d339319edbfbbf53a4d1bb513585ecfe34
Zhao Qi <qi.zhao1991@gmail.com>
2020-03-01 21:51:04 +0100

    Create rgb_2_gray.py

---
+++ rgb_2_gray.py
@@ -0,0 +1,21 @@
+def BGR2GRAY(image):
+    """Transform a bgr image to a gray image. Grayscale is a kind of image luminance 
+    expression method and is calculated by the following formula: 
+    Y = 0.2126*R + 0.7152*G + 0.0722*B
+
+    Args:
+        image: uint8 ndarray data 
+    """
+    if not isinstance(image, np.ndarray):
+        raise ValueError('Image data type is not ndarray')
+
+    image = image.astype(np.float)
+
+    b = image[:, :, 0].copy()
+    g = image[:, :, 1].copy()
+    r = image[:, :, 2].copy()
+
+    image_out = 0.2126*r + 0.7152*g + 0.0722*b
+    image_out = image_out.astype(np.uint8)
+
+    return image_out
