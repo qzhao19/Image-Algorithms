6bc329f2ed28b976a5f17bd61327ac4c057e017f
Zhao Qi <qi.zhao1991@gmail.com>
2020-03-30 23:04:30 +0200

    Update binarization.py

--- binarization.py
+++ binarization.py
@@ -5,11 +5,13 @@ Created on Thu Mar  5 21:46:09 2020
 @author: qizhao
 """
 
-import numpy as np
 
-def binarization(image, threshold):
-    image[image < threshold] = 0
-    image[image > threshold] = 255
+def binarization(img_in, threshold=155):
     
-    return image
+    img = img_in.copy()
+    
+    img[img < threshold] = 0
+    img[img > threshold] = 255
+    
+    return img
 
