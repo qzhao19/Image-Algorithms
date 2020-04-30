f3c162dbdc10c3e1a9d062c3ba7600a2eb4bc70f
zhaoqi19 <qi.zhao1991@gmail.com>
2020-04-30 23:33:57 +0200

    add mean_filter method

---
+++ image_filter/mean_filter.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 23:20:03 2020
+
+@author: qizhao
+"""
+
+
+import cv2
+import numbers
+import numpy as np
+from utils.utils import display_image
+
+
+
+def _mean_filter(img_in, ksize=3):
+    """Mean image filter
+    
+
+    Parameters
+    ----------
+        img : ndarray of shape [height, width, channel]
+            input image.
+        ksize : int
+            kernel size.
+
+    Returns
+    -------
+        ndarray of shape [h, w, c].
+
+    """
+    
+    img_in = img_in.astype(np.float32)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
