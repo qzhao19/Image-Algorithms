8bb319fe92b0b863d69b8bd57851df4888b9d35f
zhaoqi19 <qi.zhao1991@gmail.com>
2020-06-28 23:52:19 +0200

    update gaussian filter

--- image_filter/gaussian_filter.py
+++ image_filter/gaussian_filter.py
@@ -9,7 +9,77 @@ Created on Mon Apr  6 23:04:43 2020
 import cv2
 import numbers
 import numpy as np
-from utils.utils import display_image
+# from .utils.utils import display_image
+
+
+
+class GaussianFilter(object):
+    """
+    """
+    def __init__(self, ksize=3, sigma=1.5):
+        self._ksize = ksize
+        self._sigma = sigma
+
+    def _gaussian_fn(self, x, y):
+        """Return a 2d gaussian distribution with mu = 0 and sigma = sigma 
+
+        Parameters:
+        ----------
+            x: int
+                The height of input image 
+            y: int
+                The width of input image 
+            
+        """
+        return (1 / (2 * np.pi * np.power(self._sigma, 2))) * \
+            np.exp(-(np.power(x, 2) + np.power(y, 2)) / (2 * np.power(self._sigma, 2)))
+
+    def _gaussian_kernel(self):
+        """Return a 2d-array of shape [ksize, ksize]
+        """
+        ksize = self._ksize
+        pad =  ksize // 2
+        kernel = np.zeros((ksize, ksize))
+
+        for y in range(-pad, -pad + ksize):
+            for x in range(-pad, -pad + ksize):
+                kernel[y + pad, x + pad] = self._gaussian_fn(x, y)
+        
+        kernel /= kernel.sum()
+        self._kernel = kernel
+
+    def _gaussian_filter(self, X):
+        """Compute gaussian filter
+
+        Parameters
+        ----------
+            X : np.ndarray of shape [height, width, channel]
+                input image.
+        Returns
+        -------
+            self class.
+        """
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def _gaussian_fn(x, y, sigma):
     """compute 2D gaussian function
@@ -160,6 +230,7 @@ def gaussian_filter(img_path, ksize = 3, sigma = 1.5):
     except:
         raise ValueError('Cannot read image from {}, check input path!'.format(img_path))
     
+    # print(img_in)
     
     if len(img_in.shape) == 1:
         img_in = cv2.cvtColor(img_in, cv2.COLOR_GRAY2RGB)
@@ -183,9 +254,9 @@ def gaussian_filter(img_path, ksize = 3, sigma = 1.5):
     return result   
     
     
-# if __name__ == '__main__':
-#     res = gaussian_filter('./image/lenna.png')
-#     display_image(res)   
+if __name__ == '__main__':
+    res = gaussian_filter('C:\\Users\\qizha\\Documents\\GitHub\\Image-Algorithms\\image\\lenna.png')
+    # display_image(res)   
     
     
     
